cd1953c784fb71ce10f744b0614fbbf3f1fc1568
JvSRS97 <jvictoreis@hotmail.com>
2022-02-24 03:47:52 -0300

    seventh commit

--- src/main/java/jvreis/webscrapingtest/Scrap.java
+++ src/main/java/jvreis/webscrapingtest/Scrap.java
@@ -25,6 +25,7 @@ public class Scrap {// classe que contém os dados HTML
 		}
 		// Elementos são um array e o .getElementsByClass pegará os elementos com a
 		// classe informada
+		
 		for (Element elemento : documento.getElementsByClass("tb-pontos-de-alagamentos")) {// pegar pela classe da
 																							// tabela (está no arquivo
 																							// "Ideia para WebScraping
@@ -33,12 +34,18 @@ public class Scrap {// classe que contém os dados HTML
 			// houver alagamento na data
 			System.out.print("Data " + Dia + "/" + Mes + "/" + Ano + " --> ");// imprimindo a data
 
+			//Trabalhar essa parte da Zona
+			//Elements zona = documento.getElementsByClass("tit-bairros");//classe que contém a zona do alagamento
+			//System.out.print("Zona: " + zona.text()+" ");// OK! Imprimindo apenas o bairro
+			
 			Elements bairro = elemento.getElementsByClass("bairro arial-bairros-alag linha-pontilhada");// pega pela
 																										// classe do
 																										// bairro //
 																										// tabela
 			// System.out.println(bairro);//imprime a tag com o bairro
 			System.out.print("Bairro: " + bairro.text());// OK! Imprimindo apenas o bairro
+			
+			
 			Elements dados_1 = elemento.getElementsByClass("arial-descr-alag col-local");// classe que contém
 																							// informações como hora de
 																							// início, hora final e
@@ -47,7 +54,8 @@ public class Scrap {// classe que contém os dados HTML
 			// imprime "De xx:xx a xx:xx Rua A"
 			// caso tenha mais de um alagamento no dia, imprime da seguinte forma:
 			// "De xx:xx a xx:xx Rua A De xx:xx a xx:xx Rua B"
-
+						
+			//passando dados para string texto que será exportada
 			texto += "\n\nLink: \n"+link+"\nData " + Dia + "/" + Mes + "/" + Ano + " --> "+"Bairro: " + bairro.text()+"; Descrição: " + dados_1.text() + "\n\n";
 		}
 		return texto;
