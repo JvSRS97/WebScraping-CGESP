48f9a2dacbc1d4cc14ba856f79fd9064937bebc9
JvSRS97 <jvictoreis@hotmail.com>
2022-02-24 02:58:27 -0300

    sixth commit

--- src/main/java/jvreis/webscrapingtest/Scrap.java
+++ src/main/java/jvreis/webscrapingtest/Scrap.java
@@ -1,15 +1,20 @@
 package jvreis.webscrapingtest;
 
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
 import java.io.OutputStreamWriter;
-import java.util.GregorianCalendar;
-
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;//importado ao criar variável "documento"
 import org.jsoup.nodes.Element;//importado ao criar variável "elemento"
+import org.jsoup.select.Elements;
 
 public class Scrap {// classe que contém os dados HTML
-	public static void search(GregorianCalendar calendar, String link, OutputStreamWriter writer) {// função para buscar
-																									// no site
+	static String texto = " ";
+	// função para buscar no site. Cria uma cópia HTML da URL e buscará elementos pela classe dada
+	//imprimirá os dados e retornará uma string texto para escrita em arquivo
+	public static String search(String Dia, String Mes, String Ano, String link) {
+		
 		Document documento = null;
 		// System.out.println(link);//testando impressão
 		try {
@@ -18,10 +23,33 @@ public class Scrap {// classe que contém os dados HTML
 		} catch (Exception e) {
 			System.out.println("Site Indisponível ou falha na requisição!");
 		}
-
+		// Elementos são um array e o .getElementsByClass pegará os elementos com a
+		// classe informada
 		for (Element elemento : documento.getElementsByClass("tb-pontos-de-alagamentos")) {// pegar pela classe da
-																							// tabela
-			System.out.println(link);// testando impressão
+																							// tabela (está no arquivo
+																							// "Ideia para WebScraping
+																							// CGESP.txt")
+			System.out.println("Link: \n" + link);// testando impressão. Só irá imprimir o link se
+			// houver alagamento na data
+			System.out.print("Data " + Dia + "/" + Mes + "/" + Ano + " --> ");// imprimindo a data
+
+			Elements bairro = elemento.getElementsByClass("bairro arial-bairros-alag linha-pontilhada");// pega pela
+																										// classe do
+																										// bairro //
+																										// tabela
+			// System.out.println(bairro);//imprime a tag com o bairro
+			System.out.print("Bairro: " + bairro.text());// OK! Imprimindo apenas o bairro
+			Elements dados_1 = elemento.getElementsByClass("arial-descr-alag col-local");// classe que contém
+																							// informações como hora de
+																							// início, hora final e
+																							// localidade
+			System.out.println("; Descrição: " + dados_1.text() + "\n\n");// OK! Imprimindo os dados
+			// imprime "De xx:xx a xx:xx Rua A"
+			// caso tenha mais de um alagamento no dia, imprime da seguinte forma:
+			// "De xx:xx a xx:xx Rua A De xx:xx a xx:xx Rua B"
+
+			texto += "\n\nLink: \n"+link+"\nData " + Dia + "/" + Mes + "/" + Ano + " --> "+"Bairro: " + bairro.text()+"; Descrição: " + dados_1.text() + "\n\n";
 		}
+		return texto;
 	}
 }
--- src/main/java/jvreis/webscrapingtest/ScrapingConfiguration.java
+++ src/main/java/jvreis/webscrapingtest/ScrapingConfiguration.java
@@ -7,10 +7,10 @@ import java.io.OutputStreamWriter;//para escrever arquivo
 import java.text.SimpleDateFormat;
 import java.util.GregorianCalendar;
 
-public class ScrapingConfiguration {
-	private String path = "C:\\Users\\jvict\\eclipse-workspace\\webscraping-test\\ScrapingFile.txt";// caminho para o
-																									// arquivo a ser
-	// gravado
+public class ScrapingConfiguration {//obs.: editar caminho do arquivo em caso de mudança de pasta
+	private String path = "C:\\Users\\jvict\\eclipse-workspace\\webscraping-test\\ScrapingFile.txt";
+	// caminho para o arquivo a ser gravado
+	
 	private String link;// link para o WebScraping
 	private OutputStreamWriter writer;// para escrever no arquivo
 
@@ -28,7 +28,22 @@ public class ScrapingConfiguration {
 				arquivo.createNewFile();// criando arquivo
 			}
 			writer = new OutputStreamWriter(new FileOutputStream(arquivo));// para escrever arquivo
-			writer.write("Zona\tBairro\tRua\tHora-Inicial\tHora-Final");// escrevendo cabeçalho
+			writer.write("Zona\tBairro\tRua\tHora-Inicial\tHora-Final");// escrevendo cabeçalho no arquivo
+			writer.flush();// limpar memória do writer
+		} catch (Exception e) {
+			e.printStackTrace();// imprime informações sobre erro, caso ocorra
+		}
+	}
+	// ------------------------------------------------------------------
+	private void escreverFile(String texto) {//função para escrever texto no arquivo
+		try {
+			File arquivo = new File(path);// declarando variável do tipo File chamada "arquivo" com o caminho "path"
+											// declarado lá em cima
+			if (!arquivo.exists()) {// se arquivo não existe [note "!" antes]
+				arquivo.createNewFile();// criando arquivo
+			}
+			writer = new OutputStreamWriter(new FileOutputStream(arquivo));// para escrever arquivo
+			writer.write(texto);
 			writer.flush();// limpar memória do writer
 		} catch (Exception e) {
 			e.printStackTrace();// imprime informações sobre erro, caso ocorra
@@ -37,6 +52,7 @@ public class ScrapingConfiguration {
 
 	// ------------------------------------------------------------------
 	private void executar() {
+		String Texto_Arquivo;
 		GregorianCalendar calendar = new GregorianCalendar();// precisa do dia, do mês e do ano de uma determianda data
 																// por conta do formato de busca do site
 																// "https://www.cgesp.org/v3/alagamentos.jsp?dataBusca=23%2F02%2F2022&enviaBusca=Buscar"
@@ -45,13 +61,21 @@ public class ScrapingConfiguration {
 		SimpleDateFormat dia = new SimpleDateFormat("dd");
 		SimpleDateFormat mes = new SimpleDateFormat("MM");
 		SimpleDateFormat ano = new SimpleDateFormat("yyyy");
-
-		while (true) {
-			formatarLink(dia.format(calendar.getTime()), mes.format(calendar.getTime()),
-					ano.format(calendar.getTime()));// chamando função para formatar link [função getTime pega a data
-													// gerada]
-			Scrap.search(calendar, link, writer);
+		//para converter SimpleDateFormat em String, usar a função: .format(GregorianCalendar.getTime())
+		String Dia, Mes, Ano;
+		int contador = 1;
+		while (contador<=31) {//pega histórico de 1 mês
+			//convertendo SimpleDateFormat em String
+			//[função .getTime pega a data gerada]
+			Dia = dia.format(calendar.getTime());
+			Mes = mes.format(calendar.getTime());
+			Ano = ano.format(calendar.getTime());
+			
+			formatarLink(Dia, Mes, Ano);// chamando função para formatar link 
+			Texto_Arquivo = Scrap.search(Dia, Mes, Ano, link);//chamando função search da classe Scrap
+			escreverFile(Texto_Arquivo);
 			calendar.add(GregorianCalendar.DATE, -1);//decrementando da data
+			contador +=1;
 		}
 	}
 
@@ -60,5 +84,6 @@ public class ScrapingConfiguration {
 		this.link = "https://www.cgesp.org/v3/alagamentos.jsp?dataBusca=" + dia + "%2F" + mes + "%2F" + ano
 				+ "&enviaBusca=Buscar";
 	}
+
 
 }
